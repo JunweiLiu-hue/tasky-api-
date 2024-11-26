9810a353d5b9c91facd4e1229946eb9b57859ca7
unknown <20109222@mail.wit.ie>
2024-11-26 14:37:58 +0000

    Mongo lab Error Handling

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT;
+const errHandler = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(500).send('Something went wrong!');
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack}`);
+};
+
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
 
+app.use(errHandler);
+
 app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+  console.info(`Server running at http://localhost:${port}`);
 });
